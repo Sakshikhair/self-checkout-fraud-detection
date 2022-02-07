c4fa9d1acfafdb5b55bc13b1ceab187e6ddbb1d2
Amaan Kamil <amaankamil7@gmail.com>
2022-02-07 10:53:25 +0530

    Fixed database issues

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, redirect, session, request
 import mysql.connector
 import os
 
 app = Flask(__name__)
 app.secret_key=os.urandom(24)
 
-# conn = mysql.connector.connect(host="", user="", password="", database="")
-# cursor = conn.cursor()
+conn = mysql.connector.connect(host="Host_name", user="user", password="password", database="database")
+cursor = conn.cursor()
 
 @app.route("/")
 def login():
@@ -24,32 +24,35 @@ def home():
         return redirect('/')
 
 #TO LOGIN
-# @app.route("/login_validation", methods=['POST'])
-# def login_validation():
-#     email = request.form.get('email')
-#     password = request.form.get('password')
-#     cursor.execute("""SELECT * from `[table-name]` WHERE `email` LIKE '{}' AND `password` LIKE '{}'""".format(email, password))
-#     users = cursor.fetchall()
+@app.route("/login_validation", methods=['POST'])
+def login_validation():
+    lemail = request.form.get('login_email')
+    lpassword = request.form.get('login_password')
+    sql = "SELECT * from users WHERE email = %s AND password = %s"
+    cursor.execute(sql, [lemail, lpassword])
+    users = cursor.fetchall()
+    print(users)
 
-#     if len(users)>0:
-#         session['user_id'] = users[0][0]
-#         return redirect('/home')
-#     else:
-#         return redirect('/')
+    if len(users)>0:
+        session['user_id'] = users[0][0]
+        return redirect('/home')
+    else:
+        return redirect('/')
 
 #TO REGISTER
-# @app.route("/add_user", methods=['POST'])
-# def add_user():
-#     name=request.form.get('uname')
-#     email=request.form.get('uemail')
-#     password=request.form.get('upassword')
-
-#     cursor.execute("""INSERT INTO `[table-name]` (`userid`, `name`, `email`, `password`) VALUES (NULL, `{}`, `{}`, `{}`).format(name, email, password)""")
-#     conn.commit()
-#     cursor.execute("""SELECT * FROM `[table-name]` WHERE `email` LIKE `{}`""".format(email))
-#     myuser=cursor.fetchall()
-#     session['user_id']=myuser[0][0]
-#     return redirect('/home')
+@app.route("/add_user", methods=['POST'])
+def add_user():
+    rname=request.form.get('register_name')
+    remail=request.form.get('register_email')
+    rpassword=request.form.get('register_password')
+    sql = "INSERT INTO users(userid, name, email, password) VALUES (NULL, %s, %s, %s)"
+    cursor.execute(sql, [rname, remail, rpassword])
+    conn.commit()
+    query = "SELECT * FROM users WHERE email = %s"
+    cursor.execute(query, [remail])
+    myuser=cursor.fetchall()
+    session['user_id']=myuser[0][0]
+    return redirect('/home')
 
 @app.route("/logout")
 def logout():
